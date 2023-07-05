f3434550c5996300ebfd6c48bbd67abea86c39c0
richie nolan <richienolan23@gmail.com>
2023-07-05 14:17:40 +0000

    add functions to JS to load the board onload and update the color to black when initialized

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,25 @@
 
 // snake board - credit to ImKennyYip on GitHub: creates the borad in the canvas on the HTML document
 var blockSize = 25;
-var rows = 30;
-var cols = 20;
+var rows = 20;
+var cols = 30;
 var board;
 var context;
 
-// snake head - credit to ImKennyYip on GitHub: adds the location of the snake head onto the board at the X and Y coordinates
-var snakeX = blockSize * 5;
-var snakeY = blockSize * 5;
+
+
+// laods the board onto the page when the page is initialized - credit to ImKennyYip on GitHub
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = rows * blockSize;
+    board.width = cols * blockSize;
+    context = board.getContext("2d"); //used to draw on the board
+
+    update();
+}
+
+// colors the board black upon loading - credit to ImKennyYip on GitHub
+function update() {
+    context.fillStyle="black";
+    context.fillRect(0, 0, board.width, board.height);
+}
